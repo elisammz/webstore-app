b288f9ec1ae109e4e1862103c6876c9d1852b22b
Elisa Mendoza <elisa.zamarripa89@gmail.com>
2022-03-23 21:05:35 +0900

    Improved animation in hero component

--- src/components/styles/TextAnimation.styled.js
+++ src/components/styles/TextAnimation.styled.js
@@ -1,26 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import styled, { keyframes } from "styled-components";
 
 export default function TextAnimation() {
+  const [count, setCount] = useState(0);
+  const [title, setTitle] = useState("");
+
   return (
     <Wrapper>
-      <h1>that saves lives</h1>
+      {words
+        .filter((word) => word.id === 2)
+        .map((word) => (
+          <h1>{word.message}</h1>
+        ))}
     </Wrapper>
   );
 }
 
+const words = [
+  {
+    id: 1,
+    message: "that saves lives",
+  },
+  {
+    id: 2,
+    message: "with scientific rationale",
+  },
+];
+
 const animation = keyframes`
-0% {opacity: 0; transform: translateY(-100px) skewY(10deg) skewX(10deg) rotateZ(30deg); filter:blur(10px);}
-25% {opacity: 1; transform: translateY(0) skewY(0deg) skewX(0deg) rotateZ(0deg); filter:blur(0px);}
-75% {opacity: 1; transform: translateY(0) skewY(0deg) skewX(0deg) rotateZ(0deg); filter:blur(0px);}
-100% {opacity: 0; transform: translateY(-100px) skewY(10deg) skewX(10deg) rotateZ(30deg); filter:blur(10px);}
+0% {opacity: 1; transform: translateY(0); }
+50% {opacity: 0; transform: translateY(-50px); }
+100% {opacity: 1; transform: translateY(0px) ; }
 `;
 
 const Wrapper = styled.span`
   opacity: 0;
   display: inline-block;
   animation-name: ${animation};
-  animation-duration: 6s;
+  animation-duration: 4s;
   animation-fill-mode: forwards;
   animation-iteration-count: infinite;
+  overflow: hidden;
 `;
