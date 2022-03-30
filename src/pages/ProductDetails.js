b503f7fc52b3cacd56980edf0f39ea7de4a0f785
Elisa Mendoza <elisa.zamarripa89@gmail.com>
2022-03-30 22:03:17 +0900

    Added styling to ProductDetails page

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { useParams, useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 //Context
 import { ProductContext } from "../context/products";
@@ -7,6 +8,22 @@ import { CartContext } from "../context/cart";
 
 //Components
 import BrandButton from "../components/styles/BrandButton.styled";
+import Navbar from "../components/styles/Navbar.styled";
+
+const StyledProductsDetails = styled.section`
+  height: 100vh;
+  align-items: center;
+  display: flex;
+  justify-content: center;
+
+  h2 {
+    color: black;
+  }
+
+  .col-6 {
+    text-align: center;
+  }
+`;
 
 const ProductDetails = () => {
   const { id } = useParams();
@@ -24,25 +41,30 @@ const ProductDetails = () => {
   const { image: url, title, description, price } = product;
 
   return (
-    <section>
-      <div>
-        <img src={url} alt={title} width="300" />
-      </div>
-      <div>
-        <h2>{title}</h2>
-        <p2>{description}</p2>
-        <h3>Price - ${price}</h3>
-
-        <button
-          onClick={() => {
-            addToCart({ ...product, id });
-            navigate("/cart");
-          }}
-        >
-          Add to Cart
-        </button>
-      </div>
-    </section>
+    <>
+      <Navbar />
+      <StyledProductsDetails>
+        <div className="row">
+          <div className="col-6">
+            <img src={url} alt={title} width="300" />
+          </div>
+          <div className="col-6">
+            <h2>{title}</h2>
+            <p2>{description}</p2>
+            <h3>Price - ${price}</h3>
+            <BrandButton
+              secondary
+              onClick={() => {
+                addToCart({ ...product, id });
+              }}
+              to="/cart"
+            >
+              Add to Cart
+            </BrandButton>
+          </div>
+        </div>
+      </StyledProductsDetails>
+    </>
   );
 };
 
